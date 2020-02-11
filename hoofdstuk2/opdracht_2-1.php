31eb3e42e7f4ebd8ac5e6002c5ad45332e309294
Mehmet Capaz <mehmet.capaz@edu-kw1c.nl>
2020-02-11 19:53:46 +0100

    2.1 gemaakt

---
+++ hoofdstuk2/opdracht_2-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<?php
+/**
+ * User: Naam
+ * Date: Date
+ * Time: 12:06 PM
+ * File: index.php
+ */
+?>
+<html lang="en">
+
+<!-- start van de head -->
+<head>
+    <!--  declaratie van de tekenset-->
+    <meta name="description" content="">
+    <!-- omschrijving van de website -->
+    <meta name="keywords" content="">
+    <!-- keywords van de website -->
+    <meta charset="utf-8">
+    <!-- koppeling naar CSS bestand -->
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+    <title></title>
+    <!-- einde head-->
+</head>
+<title>
+
+</title>
+<body>
+
+<p>
+    <?php echo "Hello World!"; ?>
+</p>
+
+</body>
+</html>
