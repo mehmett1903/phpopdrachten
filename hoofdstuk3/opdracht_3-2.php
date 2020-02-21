ec4be589232697081a8a730cf77373f7f6e207b6
Mehmet Capaz <mehmet.capaz@edu-kw1c.nl>
2020-02-21 10:20:15 +0100

    3.2 klaar

--- hoofdstuk3/opdracht_3-2.php
+++ hoofdstuk3/opdracht_3-2.php
@@ -1,1 +1,39 @@
 <?php
+/**
+ * User: Naam
+ * Date: Date
+ * Time: 12:06 PM
+ * File: index.php
+ */
+
+$trafficLightColor = "groen";
+$ambulanceComing = false;
+$driveOn = true;
+
+if ($trafficLightColor == "groen" && $ambulanceComing == false) {
+    $driveOn = true;
+}
+else if ($trafficLightColor == "groen" && $ambulanceComing == true  ) {
+    $driveOn = false;
+}
+else if ($trafficLightColor == "oranje" && $ambulanceComing == false) {
+    $driveOn = false;
+}
+else if ($trafficLightColor == "oranje" && $ambulanceComing == true  ) {
+    $driveOn = false;
+}
+else if ($trafficLightColor == "rood" && $ambulanceComing == false) {
+    $driveOn = false;
+}
+else if ($trafficLightColor == "rood" && $ambulanceComing == true  ) {
+    $driveOn = false;
+}
+//hier weet jij of je mag doorrijden.
+
+if ($driveOn == false) {
+    echo "U moet stoppen";
+}
+else if ($driveOn == true) {
+    echo "U mag doorrijden";
+}
+?>
