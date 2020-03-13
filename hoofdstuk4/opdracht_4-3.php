6903b0fc88ae865325f148a5d30cdd825636830d
Mehmet Capaz <mehmet.capaz@edu-kw1c.nl>
2020-03-14 00:53:33 +0100

    huiswerk af

--- hoofdstuk4/opdracht_4-3.php
+++ hoofdstuk4/opdracht_4-3.php
@@ -1,1 +1,12 @@
 <?php
+include "../includes/header.php";
+include "../includes/aside.php";
+include "./script.php";
+
+echo $task2 . $text . "0" ;
+echo $task3 . $text2 . "0";
+echo $task4;
+echo "<table> <tr>" . $task5 . "</tr> </table>";
+echo $task6;
+
+include "../includes/footer.php";
